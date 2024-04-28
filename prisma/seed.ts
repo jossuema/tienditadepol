38a5c28642487ccd418e936aa538f38a208dffd2
jossuema <mmalla1@outlook.com>
2024-04-27 22:15:38 -0500

    Seed data + prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { categories } from "./data/categories";
+import { PrismaClient } from "@prisma/client";
+import { products } from "./data/products";
+
+const prisma = new PrismaClient();
+
+async function main(){
+    try{
+        await prisma.category.createMany({
+            data: categories
+        })
+        await prisma.product.createMany({
+            data: products
+        })
+    }catch(error){
+        console.log(error);
+    }
+}
+
+main().then(async () => {
+    await prisma.$disconnect();
+}).catch(async (error) => {
+    console.log(error);
+    await prisma.$disconnect();
+    process.exit(1);
+}); 
